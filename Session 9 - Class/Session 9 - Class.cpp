21268cf11aa75655047ddf7a9a5759d32f5d3138
Kigaroth <hp_20@hotmail.com>
2020-10-27 10:38:19 +0100

    Add project files.

---
+++ Session 9 - Class/Session 9 - Class.cpp
@@ -0,0 +1,19 @@
+// Session 9 - Class.cpp //
+
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <iomanip>
+
+void readFromFile();
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
+void readFromFile() {
+
+    std::fstream File("story1.txt", std::ios::in)
+
+}
